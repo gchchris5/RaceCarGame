b380e87780841e1b4cb3d9ae2f1afa47422f6377
gchchris5 <64111272+gchchris5@users.noreply.github.com>
2021-07-13 15:46:47 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MyCarRacingGame',
+    version='1',
+    packages=['RacingGame', 'RacingGame.enums'],
+    url='',
+    license='',
+    author='chrishenderson',
+    author_email='gch1994@live.com',
+    description='Car racing game'
+)
